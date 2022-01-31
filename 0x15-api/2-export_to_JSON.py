823dcb61737c871dc3c24164f8d7953cc6def2a1
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-31 18:00:18 -0500

    [UPDATE]

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
-"""Export to json info of https://jsonplaceholder.typicode.com"""
-
-import csv
+"""Gathering data from API placeholder"""
 import json
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    employee_username = ''
-
-    dict_user = {}
-    array_tasks = []
-
-    page_user = "https://jsonplaceholder.typicode.com/users/{}".format(
-        sys.argv[1])
-
-    req_user = requests.get(page_user)
-    employee_username = req_user.json()['username']
-
-    page = ("https://jsonplaceholder.typicode.com/todos?userId={}".format
-            (sys.argv[1]))
-    req = requests.get(page)
-
-    for task in req.json():
-        dict_task = {}
-        dict_task['task'] = task.get('title')
-        dict_task['completed'] = task.get('completed')
-        dict_task['username'] = employee_username
-        array_tasks.append(dict_task)
-    dict_user[str(sys.argv[1])] = array_tasks
-    json_object = json.dumps(dict_user)
-
-    with open("{}.json".format(sys.argv[1]), "w") as outfile:
-        outfile.write(json_object)
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(sys.argv[1]))
+    d = r.json()
+    name = d.get('username')
+    user_id = d.get('id')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    d_todos = r.json()
+    titles = []
+    completed = []
+    for i in d_todos:
+        if i.get('userId') == user_id:
+            titles.append(i.get('title'))
+            completed.append(i.get('completed'))
+    l = [{'username': name, 'completed': completed[i], 'task': e}
+         for i, e in enumerate(titles)]
+    json_dict = {user_id: l}
+    with open('{}.json'.format(sys.argv[1]), mode='w') as json_file:
+        json.dump({user_id: l}, json_file)
