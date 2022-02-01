206c853faaad3bef1a5c84504401dbda1c451f10
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-31 22:46:09 -0500

    [UPDATE]

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Gathering data from API placeholder"""
-import json
-import requests
-import sys
+''' Uses REST API, and for an employee ID, gives its information. '''
 
 
-if __name__ == "__main__":
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(sys.argv[1]))
-    d = r.json()
-    name = d.get('username')
-    user_id = d.get('id')
-    r = requests.get('https://jsonplaceholder.typicode.com/todos')
-    d_todos = r.json()
-    titles = []
-    completed = []
-    for i in d_todos:
-        if i.get('userId') == user_id:
-            titles.append(i.get('title'))
-            completed.append(i.get('completed'))
-    l = [{'username': name, 'completed': completed[i], 'task': e}
-         for i, e in enumerate(titles)]
-    json_dict = {user_id: l}
-    with open('{}.json'.format(sys.argv[1]), mode='w') as json_file:
-        json.dump({user_id: l}, json_file)
+if __name__ == '__main__':
+    import json
+    import sys
+    import requests
+
+    employee_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    url_user = base_url + 'users/{}'.format(employee_id)
+    url_todo = base_url + 'todos'
+    info_user = requests.get(url_user).json()
+    all_todo = requests.get(url_todo).json()
+
+    username = info_user.get('username')
+    employee_json = {}
+
+    with open('{}.json'.format(employee_id), 'w+') as file:
+        tasks = []
+        for todo in all_todo:
+            if todo.get('userId') == int(employee_id):
+                task = {}
+                task['task'] = todo.get('title')
+                task['completed'] = todo.get('completed')
+                task['username'] = username
+                tasks.append(task)
+        employee_json[employee_id] = tasks
+        file.write(json.dumps(employee_json))
